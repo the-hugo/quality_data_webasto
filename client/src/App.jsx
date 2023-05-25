33b520b75dee92be514130ff918551878030f447
the-hugo <101996060+the-hugo@users.noreply.github.com>
2023-05-25 11:18:42 +0200

    created react app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <div>
+                <p>Hi</p>
+            </div>
+        </div>
+    )
+}
+
+export default App;
