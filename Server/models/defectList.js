9a5907752dc359faf6bde7a99cc515466e20b76b
Maxeick <max.eickhoff1@web.de>
2023-06-13 12:03:28 +0200

    Update defectList.js

--- Server/models/defectList.js
+++ Server/models/defectList.js
@@ -8,7 +8,7 @@ const defectSchema = new mongoose.Schema({
   group: { type: String },
   defect_type: { type: String },
   quality_issues: { type: String },
-  need_location: { type: Number, enum: [0, 1, 2] },
+  need_location: { type: Number },
 });
 
 const DefectList = mongoose.model('DefectList', defectSchema);
