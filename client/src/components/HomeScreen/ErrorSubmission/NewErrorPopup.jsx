619496ee07cab29cf75ac29b53fba85c0b80d98e
Maxeick <max.eickhoff1@web.de>
2023-06-21 14:52:24 +0200

    Improved error Pop up

--- client/src/components/HomeScreen/ErrorSubmission/NewErrorPopup.jsx
+++ client/src/components/HomeScreen/ErrorSubmission/NewErrorPopup.jsx
@@ -1,40 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa'; // Import close icon
 
+// Creating Category Options
+const errorOptions = ['Dimension & Fit', 'Material & Documentation', 'Seal and Adheasion', 'Surface Quality']; // The array is outsourced
 
 const NewErrorPopup = ({ onClose }) => {
+  // State variables for popup open status, selected option, and text field input
   const [isOpen, setIsOpen] = useState(true);
   const [selectedOption, setSelectedOption] = useState('');
   const [textField, setTextField] = useState('');
 
+  // Function to close the popup and notify parent component
   const handleClosePopup = () => {
     setIsOpen(false);
-    onClose(); // Call the onClose function passed as prop to notify the parent component
+    onClose();
   };
 
-  const handleAnomalyClick = () => {
-    console.log("Anomaly button clicked");
-  };
+  // Functions to handle the click of each respective button
+  const handleAnomalyClick = () => { console.log("Anomaly button clicked"); };
+  const handleReworkClick = () => { console.log("Rework button clicked"); };
+  const handleScrapClick = () => { console.log("Scrap button clicked"); };
 
-  const handleReworkClick = () => {
-    console.log("Rework button clicked");
-  };
+  // Effect hook to set popup open status when the component mounts
+  useEffect(() => { setIsOpen(true); }, []);
 
-  const handleScrapClick = () => {
-    console.log("Scrap button clicked");
-  };
+  // Function to update the selected option state
+  const handleOptionChange = (option) => { setSelectedOption(option); };
 
-  useEffect(() => {
-    setIsOpen(true); // Set isOpen to true when the component mounts
-  }, []);
-
-  const handleOptionChange = (e) => {
-    setSelectedOption(e.target.value);
-  };
-
-  const handleTextFieldChange = (e) => {
-    setTextField(e.target.value);
-  };
+  // Function to update the text field input state
+  const handleTextFieldChange = (e) => { setTextField(e.target.value); };
 
   return (
     <div className="popup">
@@ -42,27 +36,42 @@ const NewErrorPopup = ({ onClose }) => {
         <div className="title-container">
           <h3 className="popup-title">Please select a new Error</h3>
         </div>
-        <div className="option-container">
-          {['Dimension', 'Material', 'Seal', 'Surface', 'Engineering'].map(option => (
-            <label key={option} style={{ marginRight: '10px', cursor: 'pointer' }}>
-              <input type="radio" value={option} checked={selectedOption === option} onChange={handleOptionChange} />
+        {/* Container for the custom radio button group */}
+        <div className="option-container" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* Mapping through the errorOptions array to create a custom radio button for each option */}
+          {errorOptions.map(option => (
+            <div 
+              key={option} 
+              onClick={() => handleOptionChange(option)} // OnClick updates the selectedOption state to the clicked option
+              // Styling for the custom radio buttons, background color changes based on the selected option
+              style={{
+                padding: '10px',
+                border: '1px solid #000',
+                borderRadius: '4px',
+                cursor: 'pointer',
+                backgroundColor: selectedOption === option ? '#aaa' : '#fff',
+                flex: 1,
+                textAlign: 'center',
+                marginRight: errorOptions.indexOf(option) === errorOptions.length - 1 ? 0 : '10px'
+              }}>
               {option}
-            </label>
+            </div>
           ))}
         </div>
-        <div className="title-container">
-          <h4 className="popup-title">Description:</h4>
-        </div>
+        {/* Container for the text input field */}
         <div className="text-field-container">
-          <input type="text" value={textField} onChange={handleTextFieldChange} placeholder="Enter text here..." style={{ margin: '10px 0', padding: '10px' }} />
+          {/* Text input field, onChange updates the textField state with the input */}
+          <textarea value={textField} onChange={handleTextFieldChange} placeholder="Enter text here..." style={{ margin: '10px 0', padding: '10px', width: '100%', height: '100px' }} />
         </div>
+        {/* Container for the buttons */}
         <div className="button-container">
+          {/* Each button has an onClick that fires the respective handleClick function */}
           <button className="anomaly-button" onClick={handleAnomalyClick}>Anomaly</button>
           <button className="rework-button" onClick={handleReworkClick}>Rework</button>
           <button className="scrap-button" onClick={handleScrapClick}>Scrap</button>
         </div>
       </div>
-
+      {/* Close button, onClick fires the handleClosePopup function */}
       <button className="close-button" onClick={handleClosePopup}>
         <FaTimes style={{ fontSize: '1.3em' }} />
       </button>
