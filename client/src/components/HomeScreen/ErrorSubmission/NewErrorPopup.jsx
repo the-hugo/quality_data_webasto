8a81c58e30022f7ed5daec30b66b2a2c5c31b912
Maxeick <max.eickhoff1@web.de>
2023-06-21 15:57:22 +0200

    X

--- client/src/components/HomeScreen/ErrorSubmission/NewErrorPopup.jsx
+++ client/src/components/HomeScreen/ErrorSubmission/NewErrorPopup.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa'; // Import close icon
 
 // Creating Category Options
-const errorOptions = ['Dimension & Fit', 'Material & Documentation', 'Seal and Adheasion', 'Surface Quality']; // The array is outsourced
+const errorOptions = ['Dimension & Fit', 'Material & Documentation', 'Seal and Adhesion', 'Surface Quality']; // The array is outsourced
+
+const errorDropdownOptions = ['Dent', 'Bubbles', 'Scratch', 'Crack']; // Dummy data for the error dropdown options
 
 const NewErrorPopup = ({ onClose }) => {
   // State variables for popup open status, selected option, and text field input
@@ -40,8 +42,8 @@ const NewErrorPopup = ({ onClose }) => {
         <div className="option-container" style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* Mapping through the errorOptions array to create a custom radio button for each option */}
           {errorOptions.map(option => (
-            <div 
-              key={option} 
+            <div
+              key={option}
               onClick={() => handleOptionChange(option)} // OnClick updates the selectedOption state to the clicked option
               // Styling for the custom radio buttons, background color changes based on the selected option
               style={{
@@ -53,22 +55,50 @@ const NewErrorPopup = ({ onClose }) => {
                 flex: 1,
                 textAlign: 'center',
                 marginRight: errorOptions.indexOf(option) === errorOptions.length - 1 ? 0 : '10px'
-              }}>
+              }}
+            >
               {option}
             </div>
           ))}
         </div>
+        {/* Dropdown for selecting different errors */}
+        <div className="dropdown-container">
+          <select
+            value={selectedOption} // Set the selected option
+            onChange={(e) => handleOptionChange(e.target.value)} // Handle option change
+            style={{ margin: '10px 0', padding: '10px', width: '100%' }}
+          >
+            <option value="">Select Error</option>
+            {/* Map through the errorDropdownOptions and create an option for each */}
+            {errorDropdownOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
         {/* Container for the text input field */}
         <div className="text-field-container">
           {/* Text input field, onChange updates the textField state with the input */}
-          <textarea value={textField} onChange={handleTextFieldChange} placeholder="Enter text here..." style={{ margin: '10px 0', padding: '10px', width: '100%', height: '100px' }} />
+          <textarea
+            value={textField}
+            onChange={handleTextFieldChange}
+            placeholder="Enter text here..."
+            style={{ margin: '10px 0', padding: '10px', width: '100%', height: '100px' }}
+          />
         </div>
         {/* Container for the buttons */}
         <div className="button-container">
           {/* Each button has an onClick that fires the respective handleClick function */}
-          <button className="anomaly-button" onClick={handleAnomalyClick}>Anomaly</button>
-          <button className="rework-button" onClick={handleReworkClick}>Rework</button>
-          <button className="scrap-button" onClick={handleScrapClick}>Scrap</button>
+          <button className="anomaly-button" onClick={handleAnomalyClick}>
+            Anomaly
+          </button>
+          <button className="rework-button" onClick={handleReworkClick}>
+            Rework
+          </button>
+          <button className="scrap-button" onClick={handleScrapClick}>
+            Scrap
+          </button>
         </div>
       </div>
       {/* Close button, onClick fires the handleClosePopup function */}
