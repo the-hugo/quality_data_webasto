d057dccb3ce71f5756e164fef7d8c71fb3719d08
Maxeick <max.eickhoff1@web.de>
2023-07-06 11:00:42 +0200

    CARDS

--- client/src/components/DataAnalytics/Dashboard2.jsx
+++ client/src/components/DataAnalytics/Dashboard2.jsx
@@ -26,34 +26,37 @@ const Dashboard2 = () => {
 
   return (
     <div className="site-card-wrapper">
-      <Row>
+      <Col span={24}>
         <MasterBar setLocationIds={setLocationIds} />
-      </Row>
+      </Col>
+
       <Row gutter={16}>
         <Col span={8}>
           <Card title="Heatmap Exact Location" bordered={true}>
             <Heatmap locationIds={locationIds} />
           </Card>
           <Card title="Line Chart 1" bordered={true}>
-            <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+            <div className="chart-container">
+              <LineChart
+                width={400}
+                height={280}
+                data={data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="pv"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+              </LineChart>
+            </div>
           </Card>
         </Col>
         <Col span={8}>
@@ -61,57 +64,76 @@ const Dashboard2 = () => {
             <HeatmapGrid locationIds={locationIds} />
           </Card>
           <Card title="Line Chart 2" bordered={true}>
-            <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+            <div className="chart-container">
+              <LineChart
+               width={400}
+               height={280}
+               data={data}
+               margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+             >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="pv"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+              </LineChart>
+            </div>
           </Card>
         </Col>
         <Col span={5}>
-        <Card
-  title={<div className="ant-card-head-title"><h2>Most Product Defects</h2></div>}
-  bordered={true}
-  className="kpi-card"
->
-  <p className="text-item">Sample Text 1</p>
-</Card>
-<Card
-  title={<div className="ant-card-head-title"><h2>Most Category Defects</h2></div>}
-  bordered={true}
-  className="kpi-card"
->
-  <p className="text-item">Sample Text 2</p>
-</Card>
-<Card
-  title={<div className="ant-card-head-title"><h2>Most Error-Types Recorded</h2></div>}
-  bordered={true}
-  className="kpi-card"
->
-  <p className="text-item">Sample Text 3</p>
-</Card>
-<Card
-  title={<div className="ant-card-head-title"><h2>KPI 4 Header</h2></div>}
-  bordered={true}
-  className="kpi-card"
->
-  <p className="text-item">Sample Text 4</p>
-</Card>
+          <Card
+            title={
+              <div className="ant-card-head-title">
+                <h2>Most Product Defects</h2>
+              </div>
+            }
+            bordered={true}
+            style={{ width: '300px', height: '200px' }}
+            className="kpi-card"
+          >
+            <p className="text-item">Sample Text 1</p>
+          </Card>
 
+          <Card
+            title={
+              <div className="ant-card-head-title">
+                <h2>Most Category Defects</h2>
+              </div>
+            }
+            bordered={true}
+            className="kpi-card"
+          >
+            <p className="text-item">Sample Text 2</p>
+          </Card>
+          <Card
+            title={
+              <div className="ant-card-head-title">
+                <h2>Most Error-Types Recorded</h2>
+              </div>
+            }
+            bordered={true}
+            className="kpi-card"
+          >
+            <p className="text-item">Sample Text 3</p>
+          </Card>
+          <Card
+            title={
+              <div className="ant-card-head-title">
+                <h2>KPI 4 Header</h2>
+              </div>
+            }
+            bordered={true}
+            className="kpi-card"
+          >
+            <p className="text-item">Sample Text 4</p>
+          </Card>
         </Col>
       </Row>
     </div>
