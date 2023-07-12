5891917f991fc8a30f77e2b259a7f245e5831b70
the-hugo <101996060+the-hugo@users.noreply.github.com>
2023-07-12 20:36:39 +0200

    lil fix

--- client/src/components/DataAnalytics/Structuring_Elements_Analytics/BarChart.jsx
+++ client/src/components/DataAnalytics/Structuring_Elements_Analytics/BarChart.jsx
@@ -1,38 +1,38 @@
-// import { useState, useEffect, useMemo } from 'react';
-// import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Line } from 'recharts';
-// import Card from 'antd/es/card/Card';
+import { useState, useEffect, useMemo } from 'react';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Line } from 'recharts';
+import Card from 'antd/es/card/Card';
 
 
-// const BarChartComp = function({ filteredData }) {
+const BarChartComp = function({ filteredData }) {
 
 
-//   return (
-//     <Card title="Bar Chart 1" bordered={true}>
-//       <div className="chart-container">
-//         <BarChart width={400} height={280} data={timeseries} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-//           <CartesianGrid strokeDasharray="3 3" />
-//           <XAxis dataKey="" />
-//           <YAxis />
-//           <Tooltip />
-//           <Legend />
-//           {keys.map((graph) => (
-//             <Bar key={graph} dataKey={graph} fill={colors[graph]} />
-//           ))}
-//           {trendLine.length > 0 && (
-//             <Line
-//               key="Trend Line"
-//               type="linear"
-//               data={trendLine}
-//               dataKey="y"
-//               stroke="#000000"
-//               strokeWidth={2}
-//               dot={false}
-//             />
-//           )}
-//         </BarChart>
-//       </div>
-//     </Card>
-//   );
-// };
+  return (
+    <Card title="Bar Chart 1" bordered={true}>
+      {/* <div className="chart-container">
+        <BarChart width={400} height={280} data={timeseries} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {keys.map((graph) => (
+            <Bar key={graph} dataKey={graph} fill={colors[graph]} />
+          ))}
+          {trendLine.length > 0 && (
+            <Line
+              key="Trend Line"
+              type="linear"
+              data={trendLine}
+              dataKey="y"
+              stroke="#000000"
+              strokeWidth={2}
+              dot={false}
+            />
+          )}
+        </BarChart>
+      </div> */}
+    </Card>
+  );
+};
 
-// export default BarChartComp;
+export default BarChartComp;
