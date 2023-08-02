a5cbaf2e05a2a99dcbe3a5701b8f051defe977b7
Maxeick <max.eickhoff1@web.de>
2023-08-02 10:50:37 +0200

    Added comments

--- client/src/components/DataAnalytics/Structuring_Elements_Analytics/BarChart.jsx
+++ client/src/components/DataAnalytics/Structuring_Elements_Analytics/BarChart.jsx
@@ -4,9 +4,11 @@ import Card from 'antd/es/card/Card';
 
 const BarChartComp = function ({ filteredData }) {
   const [data, setData] = useState([]);
-
+  // Effect hook to calculate daily counts of defect action types when filteredData changes.
   useEffect(() => {
+    // Function to calculate daily counts of defect action types.
     const calculateDailyCounts = () => {
+      // Initialize an array of objects to hold the counts for each action type.
       const counts = [
         {
           name: 'Scrap',
@@ -19,7 +21,6 @@ const BarChartComp = function ({ filteredData }) {
         {
           name: 'Rework',
           Sum: 0,
-
         },
         {
           name: 'Total',
@@ -27,17 +28,22 @@ const BarChartComp = function ({ filteredData }) {
         }
       ];
 
+      // Loop through each item in the filteredData and update the corresponding counts.
       for (const item of filteredData) {
         const { action_type } = item;
         counts.find(count => count.name === action_type).Sum += 1;
         counts.find(count => count.name === 'Total').Sum += 1;
       }
+
+      // Set the data with the calculated daily counts.
       setData(counts);
     };
 
+    // Call the calculateDailyCounts function to update the data.
     calculateDailyCounts();
   }, [filteredData]);
 
+  // Render the Bar Chart component with the data and chart settings.
   return (
     <Card title="Defect Action Types" bordered={true}>
       <BarChart width={620} height={380} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
@@ -46,10 +52,12 @@ const BarChartComp = function ({ filteredData }) {
         <YAxis />
         <Tooltip />
         <Legend />
+        {/* Render a Bar component for displaying the Sum of each action type. */}
         <Bar dataKey="Sum" fill='#333399' />
       </BarChart>
     </Card>
   );
 };
 
+// Export the BarChartComp component for use in other parts of the application.
 export default BarChartComp;
--- client/src/components/DataAnalytics/Structuring_Elements_Analytics/TimeSeries.jsx
+++ client/src/components/DataAnalytics/Structuring_Elements_Analytics/TimeSeries.jsx
@@ -6,7 +6,7 @@ const TimeSeries = ({ filteredData }) => {
   const [keys, setValues] = useState([]);
   const [trendLine, setTrendLine] = useState([]);
   const [mergedData, setMergedData] = useState([]);
-
+  // Generate random colors for each data set (graph) using useMemo.
   const colors = useMemo(() => {
     const randomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     return keys.reduce((result, graph) => {
@@ -16,31 +16,35 @@ const TimeSeries = ({ filteredData }) => {
   }, [keys]);
 
   useEffect(() => {
+    // Function to calculate the trendline of the data using linear regression.
     const calculateTrendLine = (data) => {
+      // Prepare x and y data arrays for the linear regression calculation.
       const xData = data.map((item, index) => index);
       const yData = data.map((item) => item.Total);
 
+      // Utility functions for calculations.
       const sum = (arr) => arr.reduce((a, b) => a + b, 0);
       const mean = (arr) => sum(arr) / arr.length;
       const subtractMean = (arr) => arr.map((val) => val - mean(arr));
       const multiplyArrays = (arr1, arr2) => arr1.map((val, index) => val * arr2[index]);
       const square = (num) => num * num;
 
+      // Calculate linear regression coefficients (b0 and b1).
       const xMean = mean(xData);
       const yMean = mean(yData);
-
       const xMinusMean = subtractMean(xData);
       const yMinusMean = subtractMean(yData);
-
       const b1 =
         sum(multiplyArrays(xMinusMean, yMinusMean)) / sum(xMinusMean.map((val) => square(val)));
-
       const b0 = yMean - b1 * xMean;
 
+      // Generate trendline data based on the linear regression model.
       const trendLineData = xData.map((val) => ({
         date: data[val].date,
         Trend: b0 + b1 * val,
       }));
+
+      // Set the trendline data and merge it with the original data.
       setTrendLine(trendLineData);
       const merged = data.map((item, index) => ({
         ...item,
@@ -49,8 +53,10 @@ const TimeSeries = ({ filteredData }) => {
       setMergedData(merged);
     };
 
+    // Function to fetch and process the defect data.
     const fetchDefectData = async () => {
       try {
+        // Format the date and convert it to a string (YYYY-MM-DD).
         const data = filteredData.map((item) => {
           const fullDate = new Date(item.date);
           const formattedDate = fullDate.toISOString().substring(0, 10);
@@ -60,6 +66,7 @@ const TimeSeries = ({ filteredData }) => {
           };
         });
 
+        // Group defect counts by date and error type using reduce.
         const dailyCounts = data.reduce((counts, item) => {
           const { date, error_type, category } = item;
 
@@ -99,27 +106,33 @@ const TimeSeries = ({ filteredData }) => {
           return counts;
         }, {});
 
+        // Convert the grouped data back to an array of objects with date and counts.
         const dailyCountsArray = Object.entries(dailyCounts).map(([date, counts]) => ({
           date,
           ...counts,
         }));
 
+        // Extract the keys (error types) from the data for rendering the lines in the chart.
         const keys = Object.keys(dailyCountsArray[1]);
         const index = keys.indexOf('date');
         keys.splice(index, 1);
         setValues(keys);
+
+        // Calculate and set the trendline for the data.
         calculateTrendLine(dailyCountsArray);
       } catch (error) {
         console.error('Error fetching defect data:', error);
       }
     };
 
+    // Fetch and process defect data when the filteredData prop changes.
     if (filteredData.length > 0) {
       fetchDefectData();
     }
   }, [filteredData]);
 
   return (
+    // Return a card containing the line chart with the merged data and trendline.
     <Card title="Defects Over Time" bordered>
       <LineChart width={620} height={380} data={mergedData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
@@ -128,9 +141,11 @@ const TimeSeries = ({ filteredData }) => {
         <Tooltip />
         <Legend />
         {trendLine.length > 0 && (
+          // Render the trendline as a line in the chart.
           <Line type="linear" data={mergedData} dataKey="Trend" stroke="#000000" strokeWidth={2} dot={false} />
         )}
         {keys.map((graph) => (
+          // Render each line for the error types (graphs) using the corresponding color.
           <Line key={graph} type="monotone" dataKey={graph} stroke={colors[graph]} />
         ))}
       </LineChart>
